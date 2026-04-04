db753b7ae2c00ab0477e54efea3e3688a8ebdd7c
이효준 <harry@ihyojuns-MacBook-Air.local>
2026-04-04 12:31:04 +0900

    fix swagger schema

--- backend/app/routes/product.py
+++ backend/app/routes/product.py
@@ -1,26 +1,28 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app import models
+from app import models, schemas
 
 router = APIRouter(prefix="/products", tags=["Products"])
 
 
+# 🔥 핵심 수정 부분
 @router.post("/")
-def create_product(data: dict, db: Session = Depends(get_db)):
-    product = models.Product(
-        code=data["code"],
-        name=data["name"],
-        type=data["type"],
-        location=data["location"],
-        min_stock=data.get("min_stock", 0)  # ⭐ 핵심
+def create_product(product: schemas.ProductCreate, db: Session = Depends(get_db)):
+    
+    db_product = models.Product(
+        code=product.code,
+        name=product.name,
+        type=product.type,
+        location=product.location,
+        min_stock=product.min_stock
     )
 
-    db.add(product)
+    db.add(db_product)
     db.commit()
-    db.refresh(product)
+    db.refresh(db_product)
 
-    return product
+    return db_product
 
 
 @router.get("/")
