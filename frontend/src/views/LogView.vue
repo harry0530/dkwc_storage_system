20c7416fc29cb389ca45407b1a3de34e5bd18daf
이효준 <harry@ihyojuns-MacBook-Air.local>
2026-04-09 10:43:30 +0900

    Show logs to hour precision

--- frontend/src/views/LogView.vue
+++ frontend/src/views/LogView.vue
@@ -37,8 +37,6 @@ const formatDate = (dateStr) => {
     month: "2-digit",
     day: "2-digit",
     hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
     hour12: false
   }).format(date);
 };
